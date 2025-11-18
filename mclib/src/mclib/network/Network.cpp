c4612f38a2901adb4f0981db6ca18bd24033a7a4
SomeRandoLameo <100494326+SomeRandoLameo@users.noreply.github.com>
2025-11-18 11:59:50 +0100

    Refactor NetworkInitializer and DNS resolution logic

--- mclib/src/mclib/network/Network.cpp
+++ mclib/src/mclib/network/Network.cpp
@@ -1,68 +1,65 @@
 #include <mclib/network/Network.h>
+#include <malloc.h>
 
 namespace mc {
-namespace network {
+    namespace network {
 
-class NetworkInitializer {
-private:
-public:
+        class NetworkInitializer {
+        private:
+            u32* m_SOCBuffer;
+        public:
     MCLIB_API NetworkInitializer();
     MCLIB_API ~NetworkInitializer();
 
-    NetworkInitializer(const NetworkInitializer& rhs) = delete;
-    NetworkInitializer& operator=(const NetworkInitializer& rhs) = delete;
-};
+            NetworkInitializer(const NetworkInitializer& rhs) = delete;
+            NetworkInitializer& operator=(const NetworkInitializer& rhs) = delete;
+        };
 
-#ifdef _WIN32
-NetworkInitializer::NetworkInitializer() {
-    WSADATA wsaData;
-    WSAStartup(MAKEWORD(2, 2), &wsaData);
-}
-NetworkInitializer::~NetworkInitializer() {
-    WSACleanup();
-}
-#else
-NetworkInitializer::NetworkInitializer() {
+        NetworkInitializer::NetworkInitializer()
+                : m_SOCBuffer(nullptr)
+        {
+            m_SOCBuffer = (u32*)memalign(0x1000, 0x100000);
+            if (m_SOCBuffer) {
+                Result ret = socInit(m_SOCBuffer, 0x100000);
+                if (R_FAILED(ret)) {
+                    free(m_SOCBuffer);
+                    m_SOCBuffer = nullptr;
+                }
+            }
+        }
 
-}
-NetworkInitializer::~NetworkInitializer() {
+        NetworkInitializer::~NetworkInitializer() {
+            if (m_SOCBuffer) {
+                socExit();
+                free(m_SOCBuffer);
+                m_SOCBuffer = nullptr;
+            }
+        }
 
-}
-#endif
+        NetworkInitializer initializer;
 
-NetworkInitializer initializer;
+        IPAddresses Dns::Resolve(const std::string& host) {
+            IPAddresses list;
+            addrinfo hints = { 0 }, *addresses = nullptr;
 
-IPAddresses Dns::Resolve(const std::string& host) {
-    IPAddresses list;
-    addrinfo hints = { 0 }, *addresses;
+            hints.ai_family = AF_INET;
+            hints.ai_socktype = SOCK_STREAM;
+            hints.ai_protocol = IPPROTO_TCP;
 
-    //hints.ai_family = AF_UNSPEC;
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
+            if (getaddrinfo(host.c_str(), NULL, &hints, &addresses) != 0)
+                return list;
 
-    getaddrinfo(host.c_str(), NULL, &hints, &addresses);
+            for (addrinfo *p = addresses; p != NULL; p = p->ai_next) {
+                char straddr[512];
+                inet_ntop(p->ai_family, &((sockaddr_in*)p->ai_addr)->sin_addr, straddr, sizeof(straddr));
+                list.push_back(IPAddress(straddr));
+            }
 
-    for (addrinfo *p = addresses; p != NULL; p = p->ai_next) {
-#ifdef _WIN32
-        //wchar_t straddr[35];
-        //char straddr[512];
-        //DWORD len;
-        //WSAAddressToStringA(p->ai_addr, p->ai_addrlen, NULL, straddr, &len);
+            if (addresses)
+                freeaddrinfo(addresses);
 
-        char* straddr = inet_ntoa(((sockaddr_in*)p->ai_addr)->sin_addr);
+            return list;
+        }
 
-#else
-        char straddr[512];
-
-        inet_ntop(p->ai_family, &((sockaddr_in*)p->ai_addr)->sin_addr, straddr, sizeof(straddr));
-#endif
-
-        list.push_back(IPAddress(straddr));
-    }
-
-    return list;
-}
-
-} // ns network
+    } // ns network
 } // ns mc
