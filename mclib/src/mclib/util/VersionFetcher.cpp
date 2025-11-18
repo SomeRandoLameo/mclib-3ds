5fccb4aa2c77d7a305d93c6da7974bf35d9ca644
SomeRandoLameo <100494326+SomeRandoLameo@users.noreply.github.com>
2025-11-18 12:05:22 +0100

    Added TODO

--- mclib/src/mclib/util/VersionFetcher.cpp
+++ mclib/src/mclib/util/VersionFetcher.cpp
@@ -6,64 +6,66 @@
 #include <iostream>
 
 namespace mc {
-namespace util {
+    namespace util {
 
-VersionFetcher::VersionFetcher(const std::string& host, u16 port)
-    : m_Dispatcher(),
-      m_Version(mc::protocol::Version::Minecraft_1_12_2), 
-      m_Forge(&m_Dispatcher, nullptr),
-      m_Connection(nullptr),
-      m_Host(host),
-      m_Port(port),
-      m_Found(false)
-{
+        VersionFetcher::VersionFetcher(const std::string& host, u16 port)
+            : m_Dispatcher(),
+              m_Version(mc::protocol::Version::Minecraft_1_12_2),
+              m_Forge(&m_Dispatcher, nullptr),
+              m_Connection(nullptr),
+              m_Host(host),
+              m_Port(port),
+              m_Found(false)
+        {
 
-}
-
-void VersionFetcher::OnPingResponse(const json& node) {
-    static const std::map<s32, mc::protocol::Version> mapping = {
-        { 210, mc::protocol::Version::Minecraft_1_10_2 },
-        { 315, mc::protocol::Version::Minecraft_1_11_0 },
-        { 316, mc::protocol::Version::Minecraft_1_11_2 },
-        { 335, mc::protocol::Version::Minecraft_1_12_0 },
-        { 338, mc::protocol::Version::Minecraft_1_12_1 },
-        { 340, mc::protocol::Version::Minecraft_1_12_2 },
-        { 404, mc::protocol::Version::Minecraft_1_13_2 },
-    };
-
-    auto&& versionNode = node.value("version", json());
-
-    if (versionNode.is_object()) {
-        auto&& protocolNode = versionNode.value("protocol", json());
-
-        if (protocolNode.is_number_integer()) {
-            s32 protocol = protocolNode.get<int>();
-
-            auto iter = mapping.lower_bound(protocol);
-            if (iter != mapping.end()) {
-                m_Version = iter->second;
-                m_Found = true;
-                m_Connection->Disconnect();
-            }
         }
-    }
 
-    m_Connection->Disconnect();
-}
+        void VersionFetcher::OnPingResponse(const json& node) {
+            static const std::map<s32, mc::protocol::Version> mapping = {
+                { 210, mc::protocol::Version::Minecraft_1_10_2 },
+                { 315, mc::protocol::Version::Minecraft_1_11_0 },
+                { 316, mc::protocol::Version::Minecraft_1_11_2 },
+                { 335, mc::protocol::Version::Minecraft_1_12_0 },
+                { 338, mc::protocol::Version::Minecraft_1_12_1 },
+                { 340, mc::protocol::Version::Minecraft_1_12_2 },
+                { 404, mc::protocol::Version::Minecraft_1_13_2 },
+            };
 
-mc::protocol::Version VersionFetcher::GetVersion() {
-    if (m_Found) return m_Version;
+            auto&& versionNode = node.value("version", json());
 
-    mc::core::Client m_Client(&m_Dispatcher);
+            if (versionNode.is_object()) {
+                auto&& protocolNode = versionNode.value("protocol", json());
 
-    m_Connection = m_Client.GetConnection();
+                if (protocolNode.is_number_integer()) {
+                    s32 protocol = protocolNode.get<int>();
 
-    m_Client.GetConnection()->RegisterListener(this);
-    m_Client.Ping(m_Host, m_Port, mc::core::UpdateMethod::Block);
-    m_Client.GetConnection()->UnregisterListener(this);
+                    auto iter = mapping.lower_bound(protocol);
+                    if (iter != mapping.end()) {
+                        m_Version = iter->second;
+                        m_Found = true;
+                        m_Connection->Disconnect();
+                    }
+                }
+            }
 
-    return m_Version;
-}
+            m_Connection->Disconnect();
+        }
 
-} // ns util
+        mc::protocol::Version VersionFetcher::GetVersion() {
+            if (m_Found) return m_Version;
+
+            mc::core::Client m_Client(&m_Dispatcher);
+
+            m_Connection = m_Client.GetConnection();
+
+            m_Client.GetConnection()->RegisterListener(this);
+
+            //TODO: Crash fix in here, probably socket crash, i dont know
+            m_Client.Ping(m_Host, m_Port, mc::core::UpdateMethod::Block);
+            m_Client.GetConnection()->UnregisterListener(this);
+
+            return m_Version;
+        }
+
+    } // ns util
 } // ns mc
